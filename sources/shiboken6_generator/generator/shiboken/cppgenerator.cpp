ea111de7c8b65d9377d5f73d67ca065eb5a7c4a1
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-08-05 08:54:03 +0200

    shiboken6_generator: Do not generate protected field access if there is no wrapper class
    qtcanvaspainter/b25cf5d3a192bd0ba0bfd829970cc55d38e9705d requires the
    wrapper to be disabled for QCanvasGradient; which causes a compile error
    on Windows due to the protected field access. Do not generate protected
    field access in that case.
    
    Task-number: PYSIDE-3276
    Change-Id: I46d63220c6fa297eefd4c9a0c87991834e9758e8
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/shiboken6_generator/generator/shiboken/cppgenerator.cpp
+++ sources/shiboken6_generator/generator/shiboken/cppgenerator.cpp
@@ -664,6 +664,11 @@ static void warnAboutTypes(const AbstractMetaClassCPtr &metaClass)
     }
 }
 
+static bool isProtectedField(const AbstractMetaField &field)
+{
+    return field.isProtected();
+}
+
 /// Function used to write the class generated binding code on the buffer
 /// \param s the output buffer
 /// \param classContext the pointer to metaclass information
@@ -898,8 +903,14 @@ void CppGenerator::generateClass(TextStream &s,
     }
 
     if (shouldGenerateGetSetList(metaClass)) {
-        const AbstractMetaFieldList &fields = metaClass->fields();
-        for (const AbstractMetaField &metaField : fields) {
+        AbstractMetaFieldList fields = metaClass->fields();
+        // If there is no wrapper, no protected fields can be generated.
+        const bool generateProtectedFields = !avoidProtectedHack() || classContext.useWrapper();
+        if (!generateProtectedFields) {
+            fields.erase(std::remove_if(fields.begin(), fields.end(), isProtectedField),
+                         fields.end());
+        }
+        for (const AbstractMetaField &metaField : std::as_const(fields)) {
             if (metaField.canGenerateGetter())
                 writeGetterFunction(s, metaField, classContext);
             if (metaField.canGenerateSetter())
@@ -918,7 +929,7 @@ void CppGenerator::generateClass(TextStream &s,
         s << "// Getters and Setters for " << metaClass->name() << '\n';
         s << "static PyGetSetDef " << cpythonGettersSettersDefinitionName(metaClass)
             << "[] = {\n" << indent;
-        for (const AbstractMetaField &metaField : fields) {
+        for (const AbstractMetaField &metaField : std::as_const(fields)) {
             const bool canGenerateGetter = metaField.canGenerateGetter();
             const bool canGenerateSetter = metaField.canGenerateSetter();
             if (canGenerateGetter || canGenerateSetter) {
