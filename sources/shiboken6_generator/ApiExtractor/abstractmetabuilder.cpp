11c679a3df72c5a13e7881ad75db9707cd2646cd
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-08-14 15:26:14 +0200

    shiboken6_generator: Improve handling of anonymous classes/structs/unions
    clang 22 reports anonymous classes/structs/unions with a name in
    the form "(anonymous union at ..." which show up as "Not found
    in type system" in the shiboken logs. Fix the code model to report
    an empty name as was before and reject them up front.
    
    Change-Id: I4bd2271c0a32ab637b2909b245e21a0db42e70da
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/shiboken6_generator/ApiExtractor/abstractmetabuilder.cpp
+++ sources/shiboken6_generator/ApiExtractor/abstractmetabuilder.cpp
@@ -1205,6 +1205,14 @@ AbstractMetaClassPtr AbstractMetaBuilderPrivate::traverseClass(const FileModelIt
                                                              const ClassModelItem &classItem,
                                                              const AbstractMetaClassPtr &currentClass)
 {
+    if (classItem->isAnonymous()) {
+        QString name;
+        QTextStream(&name) << "anonymous " << CodeModel::classTypeName(classItem->classType())
+            << " at " << classItem->fileName() << ':' << classItem->startLine();
+        m_rejectedClasses.insert({AbstractMetaBuilder::NotInTypeSystem, name, name, {}});
+        return nullptr;
+    }
+
     QString className = stripTemplateArgs(classItem->name());
     QString fullClassName = className;
 
@@ -1233,10 +1241,6 @@ AbstractMetaClassPtr AbstractMetaBuilderPrivate::traverseClass(const FileModelIt
         reason = AbstractMetaBuilder::GenerationDisabled;
     }
     if (reason != AbstractMetaBuilder::NoReason) {
-        if (fullClassName.isEmpty()) {
-            QTextStream(&fullClassName) << "anonymous struct at " << classItem->fileName()
-                << ':' << classItem->startLine();
-        }
         m_rejectedClasses.insert({reason, fullClassName, fullClassName, QString{}});
         return nullptr;
     }
