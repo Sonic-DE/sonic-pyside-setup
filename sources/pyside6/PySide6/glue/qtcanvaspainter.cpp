de5f6de00054a55ecdfed2e3e58bb2af140c581b
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-08-18 13:54:19 +0200

    Fix QCanvasRhiPaintDriver.grabCanvas()
    Add the QObject * context parameter, adapting to
    qtcanvaspainter/3a08c1ff44c99fa8169e4fc337b7c1b85b4e4aef.
    
    Task-number: PYSIDE-3276
    Change-Id: I5e2fac2ceb344e4148cb658acb293394570fe589
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/pyside6/PySide6/glue/qtcanvaspainter.cpp
+++ sources/pyside6/PySide6/glue/qtcanvaspainter.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 // @snippet qcpainterwidget-grabcanvas
-auto callable = %PYARG_2;
+auto callable = %PYARG_3;
 auto callback = [callable](const QImage &result)
 {
     Shiboken::GilState state;
@@ -13,5 +13,5 @@ auto callback = [callable](const QImage &result)
 
 };
 Py_INCREF(callable);
-%CPPSELF.%FUNCTION_NAME(%1, callback);
+%CPPSELF.%FUNCTION_NAME(%1, %2, callback);
 // @snippet qcpainterwidget-grabcanvas
